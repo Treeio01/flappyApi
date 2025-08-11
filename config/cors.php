2a26e4a8b9d09edcd2824a3f38fbc5fefbea7583
QuantiumCodeV <quantiumcode@mail.ru>
2025-08-11 17:00:02 +0500

    fix

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://app.your.com'],
+    'allowed_origins' => ['https://flappy.digital'],
 
     'allowed_origins_patterns' => [],
 
